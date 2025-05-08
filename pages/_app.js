0e3b09fdec3f70e33955efd549de46d32259c6f8
Jack golgart <jackson@nopiatto.com>
2025-05-08 12:06:32 -0500

    Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,21 @@ import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
+
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/basketball.svg" type="image/svg+xml" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
+
 }
 
