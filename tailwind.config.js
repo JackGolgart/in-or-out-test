6cce920addb888484a678d318ec28d9b5f78231b
Jack golgart <jackson@nopiatto.com>
2025-05-08 10:26:45 -0500

    Update 10

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/swiper/**/*.{js,ts,jsx,tsx}" // ensures Tailwind classes in Swiper are processed
   ],
   theme: {
     extend: {},
